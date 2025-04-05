15a0431e173ce2d4b296d3bcd0361859321b0659
Andrzej Połtawski <aap1980@gmail.com>
2025-04-05 13:07:29 +0200

    *

--- MiniDb.cpp
+++ MiniDb.cpp
@@ -1,20 +1,25 @@
-// MiniDb.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "include/MiniDb/Metadata/TableMetadataWriter.h"
 #include <iostream>
+#include <filesystem>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace MiniDb::Metadata;
+
+int main() {
+	std::filesystem::path current_path = std::filesystem::current_path();
+
+	TableMetadataWriter writer("Users");
+	writer.addColumn("id", "int");
+	writer.addColumn("login", "string");
+
+	std::filesystem::path file_path = current_path / "Users.md";
+	if (writer.saveToFile(file_path.string())) {
+		std::cout << "Plik metadanych zapisany.\n";
+	}
+	else {
+		std::cerr << "B³¹d zapisu pliku.\n";
+	}
+
+	return 0;
+
+	std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Users.md
@@ -0,0 +1,3 @@
+TableName:Users
+ColumnName:idType:int
+ColumnName:loginType:string
