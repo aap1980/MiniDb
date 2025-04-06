3ddb27112e0a1aeccf4e963a51e3ccaa6e365afc
Andrzej Połtawski <aap1980@gmail.com>
2025-04-06 11:01:30 +0200

    *

--- src/Table/Table.cpp
+++ src/Table/Table.cpp
@@ -13,7 +13,6 @@ namespace MiniDb::Table {
 		metadataFile = tablesPath + tableName + ".md";
 		dataFile = tablesPath + tableName + ".dat";
 		loadMetadata(metadataFile);
-		loadData(dataFile);
 		std::cout << "Successfully loaded table: " << tableName << "\n";
 	}
 
@@ -22,20 +21,16 @@ namespace MiniDb::Table {
 		return reader.loadFromFile(metadataFile, *this);
 	}
 
-	bool Table::loadData(const std::string& dataFile) {
-		return true;
-	}
-
 	void Table::addRow(const std::vector<std::string>& row) {
 		if (row.size() != columns.size()) {
 			std::cerr << "Error: the number of data in a row does not match the number of columns.\n";
 			return;
 		}
-		rows.push_back(row);
-		saveDataToFile(dataFile);
+		std::vector<std::vector<std::string>> rows = { row };
+		saveDataToFile(dataFile, rows);
 	}
 
-	bool Table::saveDataToFile(const std::string& filename) const {
+	bool Table::saveDataToFile(const std::string& filename, const std::vector<std::vector<std::string>>& rows) const {
 		std::ofstream file(filename, std::ios::app);
 		if (!file.is_open()) {
 			std::cerr << "Error opening file: " << filename << std::endl;
@@ -53,19 +48,38 @@ namespace MiniDb::Table {
 		return true;
 	}
 
+	void Table::selectAll() const {
+		std::ifstream file(dataFile);
+		if (!file.is_open()) {
+			std::cerr << "Error opening data file: " << dataFile << std::endl;
+			return;
+		}
+
+		std::string line;
+		while (std::getline(file, line)) {
+			std::vector<std::string> row;
+			size_t pos = 0;
+			while ((pos = line.find(static_cast<char>(MiniDb::SEP))) != std::string::npos) {
+				row.push_back(line.substr(0, pos));
+				line.erase(0, pos + 1);
+			}
+			row.push_back(line);
+			for (const auto& data : row) {
+				std::cout << data << "\t";
+			}
+			std::cout << "\n";
+		}
+
+		file.close();
+	}
+
 	void Table::printTable() const {
 		std::cout << "Tabela: " << tableName << "\n";
 		for (const auto& column : columns) {
 			std::cout << column.name << "\t";
 		}
 		std::cout << "\n";
-
-		for (const auto& row : rows) {
-			for (const auto& data : row) {
-				std::cout << data << "\t";
-			}
-			std::cout << "\n";
-		}
+		selectAll();
 	}
 
 }
--- src/Table/Table.h
+++ src/Table/Table.h
@@ -12,14 +12,14 @@ namespace MiniDb::Table {
 		std::string dataFile;
 		bool loadMetadata(const std::string& metadataFile);
 		bool loadData(const std::string& dataFile);
-		bool saveDataToFile(const std::string& filename) const;
+		bool saveDataToFile(const std::string& filename, const std::vector<std::vector<std::string>>& rows) const;
 
 	public:
 		std::vector<MiniDb::Table::Column> columns;
-		std::vector<std::vector<std::string>> rows;
 		Table(const std::string& tableName);
 		void addRow(const std::vector<std::string>& row);
 		void printTable() const;
+		void selectAll() const;
 	};
 
 }
