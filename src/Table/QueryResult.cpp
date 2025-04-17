319a010c13fce6913b4e851e3897b643ebe73ceb
Andrzej Połtawski <aap1980@gmail.com>
2025-04-17 16:36:13 +0200

    Update QueryResult.cpp

--- src/Table/QueryResult.cpp
+++ src/Table/QueryResult.cpp
@@ -7,16 +7,26 @@
 namespace MiniDb::Table {
 
 	void QueryResult::print() const {
-		std::cout << "QueryResult" << "\n";
+		std::cout << "QueryResult\n";
 
+		// Kolumny
+		bool first = true;
 		for (const auto& column : columns.getColumns()) {
-			std::cout << std::setw(TEXT_DISPLAY_LIMIT) << MiniDb::Utils::StringUtils::shortenText(column.name, TEXT_DISPLAY_LIMIT) << "  ";
+			if (!first) std::cout << "  ";
+			std::cout << std::setw(TEXT_DISPLAY_LIMIT)
+				<< MiniDb::Utils::StringUtils::shortenText(column.name, TEXT_DISPLAY_LIMIT);
+			first = false;
 		}
 		std::cout << "\n";
 
+		// Wiersze
 		for (const auto& row : rows.getRows()) {
+			first = true;
 			for (const auto& value : row.getValues()) {
-				std::cout << std::setw(TEXT_DISPLAY_LIMIT) << MiniDb::Utils::StringUtils::shortenText(value, TEXT_DISPLAY_LIMIT) << "  ";
+				if (!first) std::cout << "  ";
+				std::cout << std::setw(TEXT_DISPLAY_LIMIT)
+					<< MiniDb::Utils::StringUtils::shortenText(value, TEXT_DISPLAY_LIMIT);
+				first = false;
 			}
 			std::cout << "\n";
 		}
